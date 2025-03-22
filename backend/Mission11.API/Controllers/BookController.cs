6dbad14817a092e30f68e6c7ec84134ce21984ed
madierickson <emadisom@byu.edu>
2025-03-21 18:40:19 -0600

    Updated feature XYZ

--- backend/Mission11.API/Controllers/BookController.cs
+++ backend/Mission11.API/Controllers/BookController.cs
@@ -14,9 +14,19 @@ namespace Mission11.API.Controllers
         public BookController(BookDbContext temp) => _context = temp;
         
         [HttpGet("AllBooks")]
-        public IEnumerable<Book> GetBooks()
+        public IActionResult GetBooks(int pageHowMany = 5, int pageNum = 1)
         {
-            return _context.Books.ToList();
+            var something = _context.Books
+                .Skip((pageNum-1) * pageHowMany)
+                .Take(pageHowMany)
+                .ToList();
+            
+            var totalNumBooks = _context.Books.Count();
+            return Ok(new
+            {
+                Books = something,
+                TotalNumBooks = totalNumBooks
+            });
         }
     }
 }
--- frontend/src/BookList.tsx
+++ frontend/src/BookList.tsx
@@ -4,35 +4,74 @@ import { Book } from './types/Book';
 function BookList() {
   const [books, setBooks] = useState<Book[]>([]);
 
+  const [pageSize, setPageSize] = useState<number>(5);
+
+  const [pageNum, setPageNum] = useState<number>(1);
+
   useEffect(() => {
     const fetchBooks = async () => {
-      const response = await fetch('https://localhost:5000/Book/AllBooks');
+      const response = await fetch(
+        `https://localhost:5000/Book/AllBooks?pageHowMany=${pageSize}&pageNum=${pageNum}`
+      );
       const data = await response.json();
       setBooks(data);
     };
 
     fetchBooks();
-  }, []);
+  }, [pageSize, pageNum]);
 
   return (
     <>
       <h1>Book List</h1>
       <br />
       {books.map((b) => (
-        <div id="bookCard">
-          <h3>{b.title}</h3>
-
-          <ul>
-            <li>Author: {b.author}</li>
-            <li>Publisher: {b.publisher}</li>
-            <li>ISBN: {b.isbn}</li>
-            <li>Classification: {b.classification}</li>
-            <li>Category: {b.category}</li>
-            <li>Page Count: {b.pageCount} pages</li>
-            <li>Price: ${b.price}</li>
+        <div id="bookCard" className="card" key={b.bookID}>
+          <h3 className="card-title">{b.title}</h3>
+          <div className="card-body"></div>
+          <ul className="list-unstyled">
+            <li>
+              <strong>Author: </strong>
+              {b.author}
+            </li>
+            <li>
+              <strong>Publisher: </strong>
+              {b.publisher}
+            </li>
+            <li>
+              <strong>ISBN: </strong>
+              {b.isbn}
+            </li>
+            <li>
+              <strong>Classification: </strong>
+              {b.classification}
+            </li>
+            <li>
+              <strong>Category: </strong>
+              {b.category}
+            </li>
+            <li>
+              <strong>Page Count: </strong>
+              {b.pageCount} pages
+            </li>
+            <li>
+              <strong>Price: </strong>${b.price}
+            </li>
           </ul>
         </div>
       ))}
+
+      <br />
+      <label>
+        Results per page:
+        <select
+          value={pageSize}
+          onChange={(p) => setPageSize(Number(p.target.value))}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </label>
     </>
   );
 }
