e5dd6a8f6d489a3a7c3c97a258d5da509c650aa0
madierickson <emadisom@byu.edu>
2025-04-04 12:35:45 -0600

    last commits for deployment

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb80fe710509c2eb4770229ec6c621aec6c2528c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+139edad46a3ca4d227206ad1f7de6de167662b99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/api/BooksAPI.ts
+++ frontend/src/api/BooksAPI.ts
@@ -5,7 +5,8 @@ interface FetchBooksResponse {
   totalNumBooks: number;
 }
 
-const API_URL = 'https://localhost:5000/Book';
+const API_URL =
+  'https://mission13-erickson-backend-ceajgzfvhdc2c0ch.eastus-01.azurewebsites.net/Book';
 
 export const fetchBooks = async (
   pageSize: number,
