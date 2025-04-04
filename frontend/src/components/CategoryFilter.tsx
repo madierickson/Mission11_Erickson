44b8dc58b69a7e2fe0c9cb2bcbda4e6acea0d073
madierickson <emadisom@byu.edu>
2025-04-04 12:45:51 -0600

    add categories

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/Book/GetBookCategories'
+          'https://mission13-erickson-backend-ceajgzfvhdc2c0ch.eastus-01.azurewebsites.net/Book/GetBookCategories'
         );
         const data = await response.json();
         console.log('Fetched categories:', data);
